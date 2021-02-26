e02726c9b45c4a41f625f8e17d473e2b0702b7e4
Aleksey <alekseyhonchar@gmail.com>
2021-02-26 18:32:38 +0200

    Stack.cs created

---
+++ lab3/lab3/Stack.cs
@@ -0,0 +1,13 @@
+﻿namespace lab3
+{
+    public class Stack
+    {
+        private int _maxLength;
+        private int _currentLength;
+        private int[] stack;
+
+        public int MaxLength => _maxLength;
+        public int CurrentLength => _currentLength;
+
+    }
+}
