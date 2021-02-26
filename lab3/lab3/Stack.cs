b9d7ed23f8b7877fd15b10ecf05c5b1cf645cea8
Aleksey <alekseyhonchar@gmail.com>
2021-02-26 18:48:15 +0200

    public Stack and some methods added
    Methods MaxSize(); CurrentPos(); Back(); Push(); Pop() and Clear()

--- lab3/lab3/Stack.cs
+++ lab3/lab3/Stack.cs
@@ -2,12 +2,34 @@
 {
     public class Stack
     {
-        private int _maxLength;
-        private int _currentLength;
+        private int _maxSize;
+        private int _currentPos;
         private int[] stack;
 
-        public int MaxLength => _maxLength;
-        public int CurrentLength => _currentLength;
+        public Stack(int length = 1024)
+        {
+            _maxSize = length;
+            _currentPos = 0;
+            stack = new int[_maxSize];
+        }
 
+        public int MaxSize => _maxSize;
+        public int CurrentPos => _currentPos;
+        public int Back => stack[_currentPos];
+        
+        public void Push(int num)
+        {
+            stack[_currentPos++] = num;
+        }
+
+        public void Pop()
+        {
+            stack[_currentPos--] = 0;
+        }
+
+        public void Clear()
+        {
+            _maxSize = 0;
+        }
     }
 }
