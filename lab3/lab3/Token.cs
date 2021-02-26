734bccc47d4147fe21d954aedf1b3f47705a003c
Aleksey <alekseyhonchar@gmail.com>
2021-02-26 19:52:23 +0200

    SetPriority() method added

--- lab3/lab3/Token.cs
+++ lab3/lab3/Token.cs
@@ -1,7 +1,34 @@
-﻿namespace lab3
+﻿using System;
+namespace lab3
 {
     class Token
     {
+        private int _priority;
+        private bool _isOperator;
+        private float _value;
+
+        private int SetPriority(string a)
+        {
+            switch (Convert.ToChar(a))
+            {
+                case '+':
+                    return 1;
+                case '-':
+                    return 1;
+                case '*':
+                    return 2;
+                case '/':
+                    return 2;
+                case '(':
+                    return 0;
+                case ')':
+                    return 0;
+                default:
+                    return 0;
+            }
+        }
+
         
+
     }
 }
