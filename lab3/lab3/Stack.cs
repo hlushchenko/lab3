a40e0e8dc31cedff9d981168e2521147b6fd1ee4
Hluushchenko <hluushchenko@gmail.com>
2021-02-26 19:03:05 +0200

    generics

--- lab3/lab3/Stack.cs
+++ lab3/lab3/Stack.cs
@@ -1,28 +1,28 @@
 ﻿namespace lab3
 {
-    public class Stack
+    public class Stack<T>
     {
         private int _maxLength;
         private int _currentPos;
-        private int[] stack;
+        private T[] stack;
 
         public Stack(int length = 1024)
         {
             _maxLength = length;
             _currentPos = 0;
-            stack = new int[_maxLength];
+            stack = new T[_maxLength];
         }
 
         public int MaxSize => _maxLength;
         public int CurrentPos => _currentPos;
-        public int Back => stack[_currentPos];
+        public T Back => stack[_currentPos];
 
-        public void Push(int num)
+        public void Push(T num)
         {
             stack[_currentPos++] = num;
         }
 
-        public int Pop()
+        public T Pop()
         {
             return stack[_currentPos - 1];
         }
