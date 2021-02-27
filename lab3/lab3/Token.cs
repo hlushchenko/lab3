a85735e4657d3f7a323cbcd757ed5991355a6817
Hluushchenko <hluushchenko@gmail.com>
2021-02-27 12:56:25 +0200

    Token.Calculate method added

--- lab3/lab3/Token.cs
+++ lab3/lab3/Token.cs
@@ -23,7 +23,12 @@ namespace lab3
                 _value = SetValue(value);
                 _priority = Convert.ToInt32(_value) / 2;
             }
+        }
 
+        public Token(float value)
+        {
+            _isOperator = false;
+            _value = value;
         }
 
         private int SetValue(string a)
@@ -46,6 +51,30 @@ namespace lab3
                     return 0;
             }
         }
-
+        public Token Calculate(Token operand1, Token operand2)
+        {
+            if (_isOperator)
+            {
+                float value = 0;
+                switch (_value)
+                {
+                    case 2:
+                        value = operand1.Value + operand2.Value;
+                        break;
+                    case 3:
+                        value = operand2.Value - operand1.Value;
+                        break;
+                    case 4:
+                        value = operand1.Value * operand2.Value;
+                        break;
+                    case 5:
+                        value = operand1.Value * operand2.Value;
+                        break;
+                }
+                return new Token(value);
+            }
+            return operand1;
+        }
     }
 }
+
